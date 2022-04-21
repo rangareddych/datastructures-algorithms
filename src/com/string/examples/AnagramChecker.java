c6cc66d1a40fdf10fd25b56066bcc40a38ba6d67
ranga <rangareddy.ch@gmail.com>
2022-04-21 23:24:35 +0100

    AnagramChecker version1

---
+++ src/com/string/examples/AnagramChecker.java
@@ -0,0 +1,30 @@
+package com.string.examples;
+
+public class AnagramChecker {
+
+    public static void main(String[] args) {
+        AnagramChecker obj = new AnagramChecker();
+        System.out.println(obj.isAnagram("", ""));
+        System.out.println(obj.isAnagram(null, null));
+        System.out.println(obj.isAnagram("abc", "bca"));
+        System.out.println(obj.isAnagram("abc", "xyz"));
+    }
+
+    private boolean isAnagram(String word1, String word2){
+        if((word1 ==null && word2 == null) || (word1.isEmpty() && word2.isEmpty()))
+            return true;
+        else if(word1.length() != word2.length()){
+            return false;
+        }
+
+        for(char ch: word1.toCharArray()){
+            int index = word2.indexOf(ch);
+            if(index == -1){
+                return false;
+            }
+            word2 = word2.substring(0, index)+word2.substring(index+1, word2.length());
+        }
+
+        return word2.isEmpty();
+    }
+}
